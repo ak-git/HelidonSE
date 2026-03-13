35635adb2f946df53008c30db7c29137c2be8567
Alexander Kobelev <ak.mail.ru@gmail.com>
2026-03-13 15:12:15 +0300

    Update dependency io.helidon:helidon-dependencies to v4.4.0

--- helidon/src/main/java/module-info.java
+++ helidon/src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module com.ak.helidon {
   requires static org.jspecify;
   requires java.logging;
-  requires io.helidon.webserver;
   requires io.helidon.logging.common;
   requires io.helidon.config;
+  requires io.helidon.config.metadata;
+  requires io.helidon.webserver;
   exports com.ak.helidon;
 }
