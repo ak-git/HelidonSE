ef67d837bbffc2cb9afdb9d8a67a95731446e8d6
Alexander Kobelev <ak.mail.ru@gmail.com>
2026-08-30 14:58:04 +0300

    Use openrewrite

--- helidon/src/test/java/com/ak/helidon/MainTest.java
+++ helidon/src/test/java/com/ak/helidon/MainTest.java
@@ -1,11 +1,12 @@
 package com.ak.helidon;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class MainTest {
   @Test
   void test() {
-    Assertions.assertDoesNotThrow(Main::main);
+    assertDoesNotThrow(Main::main);
   }
 }
